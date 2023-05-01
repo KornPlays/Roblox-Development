b56f1365bd86edd52266419bd489b6956f4b4693
KornPlays <80108357+KornPlays@users.noreply.github.com>
2023-05-01 11:11:04 +0200

    Create main.lua

---
+++ FPS Counter/main.lua
@@ -0,0 +1,11 @@
+local totalDelta=0
+local frames=0
+game:GetService("RunService").Heartbeat:Connect(function(delta)
+	totalDelta+=delta
+	frames+=1
+	if totalDelta>=1 then
+		print("FPS:",math.round(frames/totalDelta))
+		totalDelta=0
+		frames=0
+	end
+end)
